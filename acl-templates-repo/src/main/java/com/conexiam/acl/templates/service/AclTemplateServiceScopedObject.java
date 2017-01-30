49a5d6801e21838a9f977de1753e9701de80fbab
jpotts <jeffpotts01@gmail.com>
2017-01-29 22:01:32 -0600

    Add a root scoped JavaScript object to make the ACL Template service available from server-side JavaScript as "aclTemplates".

---
+++ acl-templates-repo/src/main/java/com/conexiam/acl/templates/service/AclTemplateServiceScopedObject.java
@@ -0,0 +1,32 @@
+package com.conexiam.acl.templates.service;
+
+import com.conexiam.acl.templates.exceptions.AclTemplateServiceException;
+import org.alfresco.repo.jscript.BaseScopableProcessorExtension;
+import org.alfresco.repo.jscript.ScriptNode;
+import org.apache.log4j.Logger;
+
+import java.util.Set;
+
+public class AclTemplateServiceScopedObject extends BaseScopableProcessorExtension {
+
+    static Logger logger = Logger.getLogger(AclTemplateServiceScopedObject.class);
+
+    // Dependencies
+    private AclTemplateService aclTemplateService;
+
+    public void apply(String templateId, ScriptNode scriptNode) throws AclTemplateServiceException {
+        aclTemplateService.apply(templateId, scriptNode.getNodeRef());
+    }
+
+    public Set<String> getAclTemplates() {
+        return aclTemplateService.getAclTemplates();
+    }
+
+    public Set<String> getAuthorityResolvers() {
+        return aclTemplateService.getAuthorityResolvers();
+    }
+
+    public void setAclTemplateService(AclTemplateService aclTemplateService) {
+        this.aclTemplateService = aclTemplateService;
+    }
+}
